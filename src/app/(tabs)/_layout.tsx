8526a11fbe71b5a5cbdf09f97241b6a02e54320d
David Grammatico <david.lugdunum@gmail.com>
2024-02-07 17:05:28 +0100

    add modal for cart

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
-
+import { Tabs } from 'expo-router';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@components/useColorScheme';
 import { useClientOnlyValue } from '@components/useClientOnlyValue';
@@ -46,22 +44,6 @@ export default function TabLayout() {
 							name="cutlery"
 							color={color}
 						/>
-					),
-					headerRight: () => (
-						<Link
-							href="/modal"
-							asChild>
-							<Pressable>
-								{({ pressed }) => (
-									<FontAwesome
-										name="info-circle"
-										size={25}
-										color={Colors[colorScheme ?? 'light'].text}
-										style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-									/>
-								)}
-							</Pressable>
-						</Link>
 					),
 				}}
 			/>
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,7 +9,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
-import { useColorScheme } from '@/src/components/useColorScheme';
+import { useColorScheme } from '@/components/useColorScheme';
 
 export {
 	// Catch any errors thrown by the Layout component.
@@ -59,7 +59,7 @@ function RootLayoutNav() {
 					options={{ headerShown: false }}
 				/>
 				<Stack.Screen
-					name="modal"
+					name="cart"
 					options={{ presentation: 'modal' }}
 				/>
 			</Stack>
