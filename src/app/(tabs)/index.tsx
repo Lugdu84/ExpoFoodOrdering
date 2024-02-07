451e3e89021ce6b09102c8645693f8cf9b4cf7a2
David Grammatico <david.lugdunum@gmail.com>
2024-02-07 14:50:05 +0100

    add flatlist

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,4 @@
-import { StyleSheet } from 'react-native';
-import { View } from '@components/Themed';
+import { FlatList, StyleSheet } from 'react-native';
 import products from '@assets/data/products';
 
 import ProductListItem from '@/components/ProductListItem';
@@ -8,9 +7,13 @@ const product = products[0];
 
 export default function MenuScreen() {
 	return (
-		<View>
-			<ProductListItem product={products[0]} />
-		</View>
+		<FlatList
+			data={products}
+			numColumns={2}
+			renderItem={({ item }) => <ProductListItem product={item} />}
+			columnWrapperStyle={{ gap: 10 }}
+			contentContainerStyle={{ gap: 10, padding: 10 }}
+		/>
 	);
 }
 
